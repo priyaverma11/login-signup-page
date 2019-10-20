b489ef6b0d9fdd9ac100920627dc15afdadc003a
priyaverma11 <54302176+priyaverma11@users.noreply.github.com>
2019-10-20 19:50:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function f1()
+{
+ var p = document.getElementById("a").value;
+  var q = document.getElementById("b").value;
+   var r = document.getElementById("c").value;
+  var s = document.getElementById("d").value;
+if(p==""||q==""||r==""||s=="")
+{
+	alert("All the details must be filled!");
+}
+else
+{
+	alert("You are successfully logged in!");
+}
+}
+function f2()
+{
+alert("Are you sure you want to continue with the facebook?");
+}
+function f3()
+{
+alert("Are you sure you want to continue with the twitter?");
+}
+function f4()
+{
+alert("Are you sure you want to continue with the google+?");
+}
